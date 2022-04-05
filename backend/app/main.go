45116ae542c8549c94b1fab179ecebd3be1b644a
deliciouspasta <shinyamag@outlook.jp>
2022-04-06 01:24:16 +0900

    back,front 疎通確認 pong

--- backend/app/main.go
+++ backend/app/main.go
@@ -1,45 +1,30 @@
 package main
  
 import (
-	"user-api/user-api/routes"
+	// "user-api/user-api/routes"
 
+	"github.com/gin-gonic/gin"
 	"github.com/gin-contrib/cors"
-  	"time"
+  	// "time"
 )
  
 func main() {
-	router := routes.NewRoutes()
+	// router := routes.NewRoutes()
+	// router.Run(":8888")
 
+	r := gin.Default()
+    
+    // CORS 対応
+    config := cors.DefaultConfig()
+    config.AllowOrigins = []string{"http://localhost:8080"}
+    r.Use(cors.New(config))
 
-	router.Use(cors.New(cors.Config{
-		// アクセスを許可したいアクセス元
-		AllowOrigins: []string{
-			"http://localhost:8080",
-		},
-		// アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
-		AllowMethods: []string{
-			"POST",
-			"GET",
-			"PUT",
-			"DELETE",
-			"OPTIONS",
-		},
-		// 許可したいHTTPリクエストヘッダ
-		AllowHeaders: []string{
-			"Access-Control-Allow-Credentials",
-			"Access-Control-Allow-Headers",
-			"Content-Type",
-			"Content-Length",
-			"Accept-Encoding",
-			"Authorization",
-		},
-		// cookieなどの情報を必要とするかどうか
-		AllowCredentials: true,
-		// preflightリクエストの結果をキャッシュする時間
-		MaxAge: 24 * time.Hour,
-	  }))
+    // CORS 対応の後にルーティングを書かないとうまく動かない
+    r.GET("/ping", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "pong",
+        })
+    })
 
-
-
-	router.Run(":8888")
+    r.Run(":8888")
 }
--- backend/app/user-api/routes/route.go
+++ backend/app/user-api/routes/route.go
@@ -5,11 +5,51 @@ import (
  
 	"github.com/gin-gonic/gin"
 
+	"github.com/gin-contrib/cors"
+	"time"
+
 	
 )
  
 func NewRoutes() *gin.Engine {
 	router := gin.Default()
+
+	// config := cors.DefaultConfig()
+    // config.AllowOrigins = []string{"http://localhost:8080"}
+    // router.Use(cors.New(config))
+
+
+
+	router.Use(cors.New(cors.Config{
+		// アクセスを許可したいアクセス元
+		AllowOrigins: []string{
+			"http://localhost:8080",
+		},
+		// アクセスを許可したいHTTPメソッド
+		AllowMethods: []string{
+			"POST",
+			"GET",
+			"PUT",
+			"DELETE",
+			"OPTIONS",
+		},
+		// 許可したいHTTPリクエストヘッダ
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			// "Access-Control-Allow-Origin",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+		// cookieなどの情報を必要とするかどうか
+		AllowCredentials: true,
+		// preflightリクエストの結果をキャッシュする時間
+		MaxAge: 24 * time.Hour,
+	  }))
+
+
 	router.POST("/users", handlers.NewUserHandler)
 	
 	router.GET("/users", handlers.ListUsersHandler)
